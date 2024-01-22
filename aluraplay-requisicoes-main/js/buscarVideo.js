ea8b627a5c7fb9ee5bcdbaa6c2b0ec5cc9ef03fc
Rodrigo.Luciano <brdc.cxlviii1@huawei.com>
2024-01-22 13:00:33 -0300

    selecionar pela barra de pesquisa

--- aluraplay-requisicoes-main/js/buscarVideo.js
+++ aluraplay-requisicoes-main/js/buscarVideo.js
@@ -1,11 +1,15 @@
 import { conectaApi } from "./conectApi.js";
+import constroiCard from "./mostrarVideos.js";
 
 async function buscaVideo(evento){
     evento.preventDefault()
 
     const dadosDePesquisa = document.querySelector("[data-pesquisa]").value
     const busca = await conectaApi.buscaVideo(dadosDePesquisa)
-    console.log(busca)
+    // console.log(busca)
+    const lista = document.querySelector("[data-lista]")
+    busca.forEach(elemento => lista.appendChild(
+        constroiCard(elemento.titulo,elemento.descricao,elemento.url,elemento.imagem)))
 }
 
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]")
